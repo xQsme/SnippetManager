cb96f10a0bab92b80c48096ae0ee977b231e14ed
unknown <2151163@my.ipleiria.pt>
2019-03-22 19:40:17 +0000

    single instance

--- SnippetManager/Form1.cs
+++ SnippetManager/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace SnippetManager
 {
--- SnippetManager/Program.cs
+++ SnippetManager/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +15,16 @@ namespace SnippetManager
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            bool createdNew = true;
+            using (Mutex mutex = new Mutex(true, "SnippetManager", out createdNew))
+            {
+                if (createdNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Form1());
+                }
+            }
         }
     }
 }
